b3ed3bd69966615e94ea0e71778a0dff78274841
djsy104 <148027370+djsyy@users.noreply.github.com>
2026-05-28 20:48:30 -0700

    feat: setup routing and validation rules

--- server/routes/recipeRouter.js
+++ server/routes/recipeRouter.js
@@ -14,6 +14,7 @@ import {
   updateRecipeValidation,
   deleteRecipeValidation,
 } from '../validations/recipeValidation.js';
+import recipeIngredientRouter from './recipeIngredientRouter.js';
 
 const router = express.Router();
 
@@ -21,6 +22,9 @@ router
   .route('/')
   .post(createRecipeValidation, validateRequest, createRecipe)
   .get(getRecipesValidation, validateRequest, getRecipes);
+
+router.use('/:recipeId/ingredients', recipeIngredientRouter);
+
 router
   .route('/:id')
   .get(getSingleRecipeValidation, validateRequest, getSingleRecipe)

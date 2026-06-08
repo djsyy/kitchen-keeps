cde9f6d0ae1f2c55f5bc3e9e313678f2129a2586
djsy104 <148027370+djsyy@users.noreply.github.com>
2026-06-07 18:33:35 -0700

    feat: WIP update password handling

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,10 +6,28 @@ import NotFoundError from '../errors/NotFoundError.js';
 import InternalServerError from '../errors/InternalServerError.js';
 import ConflictError from '../errors/ConflictError.js';
 
+const authDBAttributes = ['name', 'email'];
+
+// Helper function to parse and keep optional values that have been set
+const buildUpdatedProfileFields = (req) => {
+  const updatedValues = [];
+  const updatedFields = [];
+
+  authDBAttributes.forEach((attribute) => {
+    if (Object.hasOwn(req.body, attribute)) {
+      updatedValues.push(req.body[attribute]);
+      updatedFields.push(`${attribute} = $${updatedValues.length}`);
+    }
+  });
+
+  return { updatedValues, updatedFields };
+};
+
 export const register = async (req, res, next) => {
   try {
-    const { name, email, password } = req.body;
+    const { name, email, password, _confirmPassword } = req.body;
     const hashedPassword = await hashPassword(password);
+
     const result = await query(
       `
       INSERT INTO users (name, email, password_hash)
@@ -109,7 +127,93 @@ export const logout = (req, res, next) => {
   });
 };
 
-// Returns the current user
+export const updateUser = async (req, res, next) => {
+  try {
+    const userId = req.user.userId;
+    const { updatedFields, updatedValues } = buildUpdatedProfileFields(req);
+
+    updatedValues.push(userId);
+
+    const result = await query(
+      `
+      UPDATE users
+      SET ${updatedFields.join(', ')}
+      WHERE id = $${updatedValues.length}
+      RETURNING id, name, email
+      `,
+      updatedValues
+    );
+
+    const user = result.rows[0];
+    if (!user) {
+      throw new NotFoundError('User not found');
+    }
+
+    return res.status(StatusCodes.OK).json({ data: { user } });
+  } catch (error) {
+    if (error instanceof NotFoundError) {
+      return next(error);
+    }
+
+    if (error.code === '23505') {
+      return next(new ConflictError('Email already registered'));
+    }
+
+    return next(new InternalServerError('Unable to update user'));
+  }
+};
+
+export const updatePassword = async (req, res, next) => {
+  try {
+    const userId = req.user.userId;
+    const { currentPassword, newPassword, _confirmNewPassword } = req.body;
+
+    const result = await query(
+      `
+      SELECT id, password_hash
+      FROM users
+      WHERE id = $1
+      `,
+      [userId]
+    );
+
+    const user = result.rows[0];
+    if (!user) {
+      throw new NotFoundError('User not found');
+    }
+
+    const passwordMatch = await comparePasswords(
+      currentPassword,
+      user.password_hash
+    );
+
+    if (!passwordMatch) {
+      throw new UnauthorizedError('Invalid credentials');
+    }
+
+    const hashedNewPassword = await hashPassword(newPassword);
+
+    await query(
+      `
+      UPDATE users
+      SET password_hash = $1
+      WHERE id = $2
+      `,
+      [hashedNewPassword, userId]
+    );
+
+    return res.status(StatusCodes.OK).json({
+      message: 'Password updated successfully',
+    });
+  } catch (error) {
+    if (error instanceof NotFoundError || error instanceof UnauthorizedError) {
+      return next(error);
+    }
+
+    return next(new InternalServerError('Unable to update password'));
+  }
+};
+
 export const getUser = async (req, res, next) => {
   try {
     const result = await query(
